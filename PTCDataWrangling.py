7f854584e38109eeb066e40f60e3becd0fa5283c
John DeJesus <j.dejesus22@gmail.com>
2018-10-31 21:41:27 -0400

    Align code to PEP 8

--- PTCDataWrangling.py
+++ PTCDataWrangling.py
@@ -1,6 +1,6 @@
 # Data Prep for Personality Text Classifier
 
-#import libraries
+# import libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,37 +8,36 @@ import seaborn as sns
 sns.set()
 import re
 
-#import data set
-df=pd.read_csv('mbti_1.csv')
+# import data set
+df = pd.read_csv('mbti_1.csv')
 df.info()
 
-#number of columns to house each entry
-columnlist=[str(i) for i in range(50)]
+# number of columns to house each entry
+columnlist = [str(i) for i in range(50)]
 
-#split comments in posts column into idividual columns to melt
+# split comments in posts column into individual columns to melt
 df[columnlist] = df['posts'].str.split(r'\|\|\|',n=49,expand=True)
 
-
-#drop original group posts column
+# drop original group posts column
 df.drop(['posts'],inplace=True, axis=1)
 
-#put all posts into a single column called individualPosts
-#then drop variable column
-df_melt=pd.melt(df,id_vars=['type'],value_vars=columnlist, value_name='individualPosts')
+# put all posts into a single column called individualPosts
+# then drop variable column
+df_melt = pd.melt(df,id_vars=['type'],value_vars=columnlist, value_name='individualPosts')
 df_melt.drop(['variable'],inplace=True, axis=1)
 
-#filter out comments that contain links to sites and/or images
-df_melt=df_melt[df_melt['individualPosts'].str.contains('http')==False]
-#total remaining is 396313 rows
+# filter out comments that contain links to sites and/or images
+df_melt = df_melt[df_melt['individualPosts'].str.contains('http')==False]
+# total remaining is 396313 rows
 
-#check for number of nulls, result=0
-nulls=sum(df_melt['individualPosts'].isnull()==True)#
+# check for number of nulls, result=0
+nulls = sum(df_melt['individualPosts'].isnull()==True)
 
-#strip each " ' " at the beginning of each comment
+# strip each " ' " at the beginning of each comment
 df_melt['individualPosts']=df_melt['individualPosts'].str.lstrip("'")
 
-#create column for post length
+# create column for post length
 df_melt['text_length'] = df_melt['individualPosts'].apply(lambda x: len(x))
 
-#Send melted df to csv
+# Send melted df to csv
 df_melt.to_csv('mbt_1_Melted.csv',index=False)
